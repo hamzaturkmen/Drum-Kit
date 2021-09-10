0b476be34b5582707d6b982c5a0bb6ec6c21753e
hamzaturkmen <turkmen.hamza061@gmail.com>
2021-09-10 13:42:01 +0300

    complete drum kit

---
+++ index.js
@@ -0,0 +1,67 @@
+// detecting click event
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    makeSound(this.textContent);
+    animate(this.textContent);
+  });
+}
+
+// detecting keyboard event
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  animate(event.key);
+});
+
+// make sound function
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
+
+// animation function
+function animate(key) {
+  var activeButton = document.querySelector("." + key);
+  // her harf zaten bir class bunlara pressed class eklenir
+  activeButton.classList.add("pressed");
+  // belli bir süre sonra bu class kaldırılır çünkü eski halleri geri dönsünler diye...timeout
+  setTimeout(function() {
+    activeButton.classList.remove("pressed")
+  }, 100);
+}
